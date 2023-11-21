d728eb6d0fa5ae732fac00da5c62b71b7dc1b0cb
Scaffolder <scaffolder@backstage.io>
2023-11-21 13:16:07 +0700

    initial commit

---
+++ src/main/java/com/shannammtf/order/orderApplication.java
@@ -0,0 +1,13 @@
+package com.shannammtf.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class orderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
